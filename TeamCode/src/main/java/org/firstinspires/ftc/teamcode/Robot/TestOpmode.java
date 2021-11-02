ba06cc2e8bfb72e8d7aa44d890143ef3f613fb59
J-Barta <53406552+J-Barta@users.noreply.github.com>
2021-11-02 12:57:11 -0400

    Created Test (needs deletion)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/TestOpmode.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name="hello")
+public class TestOpmode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+    }
+}
